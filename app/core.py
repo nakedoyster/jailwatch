f86fe609aeb327af582047b5f519e35056e9f38e
Nartamus <artw1982@gmail.com>
2019-12-16 10:41:10 -0600

    Initial project structure
    Nothing fancy here.

---
+++ app/core.py
@@ -0,0 +1,11 @@
+def main():
+    # we're going to call this script via cron
+    # we only want to call this every 30 minutes
+    # print(inmateDB.isAlreadyThere(12))
+     #inmateDB.addInmate(12,"Lenore","Williams","female","white","110 Rivercrest","yamomma","today","toocheap","1000000","somepicurl")
+    #inmates = jailCapeCounty.Actions().start()
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='JailWatch',
+      version='1.0',
+      description='Python script to scrape inmate data and store in Db',
+      author='nakedoyster',
+      author_email='oyster@qgaba.com',
+      url='',
+      packages=['distutils', 'distutils.command'],
+     )
